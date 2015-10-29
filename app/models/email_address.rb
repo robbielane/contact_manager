24964a56ddb0f206ce7f44b280b36125867e4040
Robbie Lane <lane.robbie@gmail.com>
2015-10-28 23:12:44 -0600

    validates email address

--- app/models/email_address.rb
+++ app/models/email_address.rb
@@ -1,2 +1,3 @@
 class EmailAddress < ActiveRecord::Base
+  validates :address, :person_id, presence: true
 end
--- spec/models/email_address_spec.rb
+++ spec/models/email_address_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe EmailAddress, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:email_address) { EmailAddress.new(address: 'bob@example.com', person_id: 1)}
+
+  it 'is valid' do
+    expect(email_address).to be_valid
+  end
+
+  it 'is invalid without a address' do
+    email_address.address = nil
+    expect(email_address).to_not be_valid
+  end
+
+  it 'must have a reference a person' do
+    email_address.person_id = nil
+    expect(email_address).to_not be_valid
+  end
+
 end
